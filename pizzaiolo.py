5dd4c078ceed596f071ee6409e9e87a96da1026c
Arthur Machado <arth-ur@live.com>
2023-10-06 15:59:29 -0300

    mensagem

---
+++ pizzaiolo.py
@@ -0,0 +1,5 @@
+pizzas = ['marguerita', 'peperoni', 'mussarela']
+pizzas2 = pizzas[:]
+pizzas.append('portuguesa')
+pizzas2.remove('marguerita')
+print(f'minhas pizzas preferidas são: {pizzas}\nas do meu amigo são {pizzas2}')
