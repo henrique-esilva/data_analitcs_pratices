2f4811f8c74baf952c47023105e6225859bc4bc0
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-11-08 15:29:28 -0300

    Add files via upload

--- modulo3/mgazu0001.sql
+++ modulo3/mgazu0001.sql
@@ -2,22 +2,56 @@ create database magazu;
 
 use magazu;
 
+select * from produto;
+
+drop table produto;
 create table produto(
-cod_produto int not null auto_increment primary key,
+codproduto int not null auto_increment primary key,
 nome varChar(26) not null,
 descricao varchar(128),
 preco float not null
 );
 
+alter table produto add(codfornecedor int not null);
+
+alter table produto add constraint
+fkfornecedor foreign key(codfornecedor )
+references fornecedor(codfornecedor );
+
+drop table cliente;
 create table cliente(
-cod_cliente int not null auto_increment primary key,
+codcliente int not null auto_increment primary key,
 nome varchar(30),
 telefone varchar(14),
 endereco varchar(40)
 );
 
+alter table cliente modify telefone varchar(30) not null;
+
+drop table fornecedor;
 create table fornecedor(
-cod_fornecedor int not null auto_increment primary key,
+codfornecedor int not null auto_increment primary key,
 razao_social varchar(40) not null,
 nome_fantasia varchar(40) not null
 );
+
+drop table pedido;
+create table pedido(
+codpedido int(11) not null primary key auto_increment,
+datapedido date not null,
+quantidade int(11) not null,
+valor float not null,
+total float not null,
+codcliente int(11) not null,
+codproduto int(11) not null
+);
+
+alter table pedido add constraint
+fkproduto foreign key(codproduto)
+references produto(codproduto);
+
+
+
+insert into cliente(codcliente, nome,  telefone, endereco)
+values
+(null, 'Ana', 'Rua abc', '+552190000000');
