ba062da0fc80dee81a69a028ad191f876493903d
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-11-27 15:45:51 -0300

    Create selects.sql

---
+++ modulo3/pizzaria/selects.sql
@@ -0,0 +1,30 @@
+-- consultas no pizzaria
+
+use pizzaria;
+-- seleciona todos os valores de todos os campos da tabela
+select * from tb_pedido;
+-- seleciona apenas o maior valor do campo `preco` da tabela
+select max(preco) from tb_pedido;
+-- seleciona apenas o menor valor do campo `preco` da tabela
+select min(preco) from tb_pedido;
+-- seleciona a soma de todos os valores do campo `preco` da tabela tb_pedido
+select sum(preco) from tb_pedido;
+-- seleciona todos os valores do campo `preco` da tabela pedido em ordem crescente (ASCendente)
+select preco from tb_pedido order by preco asc;
+-- seleciona todos os valores do campo `preco` da tabela pedido em ordem decrescente (DESCendente)
+select preco from tb_pedido order by preco desc;
+
+-- documentar group by
+
+-- seleciona o somatório de todas as linhas de um campo filtrando por uma condicional
+select sum(preco) from tb_pedido where preco < 100;
+
+select nome, preco from tb_pedido inner join tb_cliente where nome like 'm%' order by nome asc;
+
+-- inner join pode ser usado para fazer a junção de tabelas durante uma consulta, desde que todas estejam relacionadas
+select nome, preco
+from tb_cliente inner join tb_pedido
+on tb_cliente.id_cliente = tb_pedido.id_cliente where preco in(45, 95, 120) order by nome desc;
+
+-- 
+select categoria, count(*) from tb_pizza group by categoria;
