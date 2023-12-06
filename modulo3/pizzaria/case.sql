76bde4175e8c4b1c2491e028ab11708f80cb3b84
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-12-06 15:42:27 -0300

    Update case.sql

--- modulo3/pizzaria/case.sql
+++ modulo3/pizzaria/case.sql
@@ -1,6 +1,6 @@
 SELECT nome, preco
 CASE 
     when preco < 50 then 'valor baixo'
-    when preco >55 then 'valor médio'
+    when preco > 55 then 'valor médio'
     else 'valor não informado'
 end as resultado  FROM tb_pizza;
