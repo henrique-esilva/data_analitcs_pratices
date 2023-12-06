0da6b3ea98ff1fae233f9d9629d55e2cd094f515
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-12-06 14:57:01 -0300

    Create escola.sql

---
+++ modulo3/06-12-2023/escola.sql
@@ -0,0 +1,29 @@
+-- facilitadora Simone
+-- analise de dados Senac
+-- 06/12/2023
+-- aluno Henrique Silva
+
+create database escola;
+use escola;
+
+create table escola(
+id int primary key,
+nome varchar(30));
+
+create table aluno(
+id int primary key,
+nome varchar(50),
+email varchar(60),
+sexo varchar(1),
+id_escola int,
+foreign key(id_escola) references escola(id));
+
+create table aluno_audit(
+id_aluno int,
+data_insert timestamp);
+
+-- cria um gatilho (trigger)
+
+create definer='root'@'localhost' trigger escola2
+before insert on aluno_audit 
+for each row set new.data_insert = now()
