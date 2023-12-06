ec4f58a67a4e31b3e6d4f825b13bdd3577ee0404
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-12-06 15:06:40 -0300

    Create case.sql

---
+++ modulo3/pizzaria/case.sql
@@ -0,0 +1,12 @@
+SELECT nome, preco
+CASE 
+    when preco < 50 then 'valor baixo'
+    when preco >55 then 'valor médio'
+    else 'valor não informado'
+end as resultado  FROM tb_pizza
+SELECT nome, preco,
+CASE 
+    when preco < 50 then 'valor baixo'
+    when preco >55 then 'valor médio'
+    else 'valor não informado'
+end as resultado  FROM tb_pizza
