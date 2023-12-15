8e09dbadac555cbe2360f15142233649df6bf00a
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-12-15 16:53:00 -0300

    Create consultas.sql

---
+++ modulo4/atividade_academia/consultas.sql
@@ -0,0 +1,12 @@
+USE academia;
+
+SELECT funcionario.nome AS 'professor', atv.nomeatividade AS 'atividade' FROM funcionario INNER JOIN professor prof ON funcionario.cpffuncionario=prof.cpffuncionario INNER JOIN habilitaprofessor rel ON rel.idprofessor=prof.idprofessor INNER JOIN atividade atv ON atv.idatividade=rel.idatividade;
+SELECT aula.dataaula, aula.horario, aluno.nome AS Aluno, aluno.telefone AS 'contato do aluno' FROM aula INNER JOIN aulaaluno ON aulaaluno.idaula=aula.idaula INNER JOIN aluno ON aluno.matricula=aulaaluno.matricula;
+SELECT
+  a.nome AS NomeAluno,
+  a.telefone AS Telefone,
+  CONCAT(e.rua, ', ', e.bairro, ', ', e.cidade, ' - ', e.uf, ' ', e.cep) AS EnderecoCompleto
+FROM
+  aluno a
+JOIN
+  endereco e ON a.cep = e.cep;
