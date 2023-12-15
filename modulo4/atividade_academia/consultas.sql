bc6b028a2d9c68830e76cca64b894241bedcf6e2
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2023-12-15 16:54:08 -0300

    Update consultas.sql

--- modulo4/atividade_academia/consultas.sql
+++ modulo4/atividade_academia/consultas.sql
@@ -1,3 +1,9 @@
+-- Mostrar nome do professor e nome das atividades que ele é habilitado
+-- Mostrar nome do aluno, telefone, data e horário da aula de todas as aulas marcadas
+-- Mostrar nome do aluno, telefone e endereço completo
+-- Mostrar nome dos produtos, valor e nome de sua categoria
+-- Mostrar nome do produto, quantidade vendida e preço vendido de todos os produtos da venda 14
+
 USE academia;
 
 SELECT funcionario.nome AS 'professor', atv.nomeatividade AS 'atividade' FROM funcionario INNER JOIN professor prof ON funcionario.cpffuncionario=prof.cpffuncionario INNER JOIN habilitaprofessor rel ON rel.idprofessor=prof.idprofessor INNER JOIN atividade atv ON atv.idatividade=rel.idatividade;
