1684dc969d7e4644495afa40668f40eee8094d62
Henrique Silva <115876954+henrique-esilva@users.noreply.github.com>
2024-02-09 14:41:07 -0300

    Delete algoritmo_de_colisao.py

--- algoritmo_de_colisao.py
+++
@@ -1,8 +0,0 @@
-def intervalo_retroativo( rect, velocidade, direcao ):
-    conjunto = [
-        [rect.left, rect.right ],
-        [rect.top,  rect.bottom],
-    ][direcao]
-    #define cálculo
-    base = conjunto[int(velocidade<0)]
-    return (base, base-velocidade)
